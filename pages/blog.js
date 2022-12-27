b223107455a73938555a01dd8c1aa31d3a88dd49
Md. Jasim <75038630+ismailjosim@users.noreply.github.com>
2022-12-28 01:41:19 +0600

    add dynamic blog post

--- pages/blog.js
+++ pages/blog.js
@@ -1,8 +1,40 @@
 
+import Link from 'next/link'
+import styles from '../styles/blog.module.css'
 
 const Blog = () => {
+
+
     return (
-        <div>This is All blog Pages</div>
+        <div className={styles.all_blogs}>
+            <div className={styles.blog_item}>
+                <h3>
+                    <Link href='/blogs/dynamic blog post one'>Welcome to blog 01</Link>
+                </h3>
+            </div>
+            <div className={styles.blog_item}>
+                <h3>
+                    <Link href='/blogs/dynamic blog post Two'>Welcome to blog 02</Link>
+                </h3>
+            </div>
+            <div className={styles.blog_item}>
+                <h3>
+                    <Link href='/blogs/dynamic blog post Three'>Welcome to blog 03</Link>
+                </h3>
+            </div>
+            <div className={styles.blog_item}>
+                <h3>
+                    <Link href='/blogs/dynamic blog post Four'>Welcome to blog 04</Link>
+                </h3>
+            </div>
+            <div className={styles.blog_item}>
+                <h3>
+                    <Link href='/blogs/dynamic blog post Five'>Welcome to blog 05</Link>
+                </h3>
+            </div>
+        </div>
+
+
     )
 }
 
--- pages/blogs/[slug].js
+++ pages/blogs/[slug].js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useRouter } from 'next/router';
-
+import Image from 'next/image';
+import styles from '../../styles/blog.module.css'
 
 const Slug = () => {
     const router = useRouter();
@@ -8,7 +9,21 @@ const Slug = () => {
 
 
     return (
-        <div>{slug}</div>
+        <div className={styles.blog}>
+            <Image
+                className={styles.img}
+                src='/blogImg01.jpg'
+                alt="Picture of the author"
+                width={500}
+                height={500}
+            />
+            <div className={styles.blog_body}>
+                <h2>Now You see {slug}</h2>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi, natus sint! Rem nihil error hic corporis itaque dicta eum assumenda, doloremque ex alias. Est, et dolorum sunt eius, odio ex quidem dolores sit ipsam labore laborum harum fugit autem distinctio laudantium, expedita quos! Possimus rem sapiente quibusdam doloremque nostrum, facilis est delectus explicabo aperiam? Quibusdam earum sequi veniam culpa quos cumque excepturi ipsa, veritatis ratione quis inventore quod minus quam? Provident, sint possimus! Cupiditate fugit iusto incidunt repellat similique non reprehenderit, dicta ad at tenetur recusandae ut! Architecto nam totam dicta exercitationem expedita, beatae quod saepe aspernatur veritatis quia tenetur.
+                </p>
+            </div>
+        </div>
     )
 }
 
